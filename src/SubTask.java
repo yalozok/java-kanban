18a4a6e3bc5de82ec7a3f26a936ff2096caf427b
yalozok <olga.kozmovskaia@yandex.ru>
2024-11-09 22:15:51 +0100

    add SubTask

---
+++ src/SubTask.java
@@ -0,0 +1,28 @@
+public class SubTask extends Task {
+    private final TaskType type = TaskType.SUB_TASK;
+    private Integer epicId;
+
+    @Override
+    public String toString() {
+        return "SubTask{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", epicId=" + epicId +
+                ", status=" + status +
+                ", type=" + type +
+                '}';
+    }
+
+    public SubTask (Integer id, String name, String description, TaskStatus status) {
+        super(id, name, description, status);
+    }
+
+    void setEpicId(Integer epicId) {
+        this.epicId = epicId;
+    }
+
+    public Integer getEpicId() {
+        return epicId;
+    }
+}
