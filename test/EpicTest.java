a5fd5f418959bfb71dc86c520eb4e27c37b8df12
yalozok <olga.kozmovskaia@yandex.ru>
2024-11-21 17:25:28 +0100

    Delete unused import

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class EpicTest {
     private static Epic epic;
     private List<Integer> subtaskIds = new ArrayList<>();
