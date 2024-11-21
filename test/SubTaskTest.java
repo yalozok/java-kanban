4174d981ac32fb1b99aeb59d781a67f545ad4a58
yalozok <olga.kozmovskaia@yandex.ru>
2024-11-21 17:21:49 +0100

    Add tests for SubTask

---
+++ test/SubTaskTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubTaskTest {
+    private static SubTask subTask;
+
+    @BeforeEach
+    public void beforeEach() {
+        subTask = new SubTask(0, "Помыть посуду", "Загрузить машинку", TaskStatus.IN_PROGRESS, 1);
+    }
+
+    @Test
+    void setEpicId() {
+        subTask.setEpicId(2);
+        Assertions.assertEquals(2, subTask.getEpicId(), "SubTask получает неверный epic id");
+    }
+
+    @Test
+    void getEpicId() {
+        Assertions.assertEquals(1, subTask.getEpicId(), "SubTask получает неверный epic id");
+    }
+
+    @Test
+    void subTaskIdShouldNotEqualsEpicId() {
+        Assertions.assertNotEquals(subTask.getId(), subTask.getEpicId(), "SubTask id и epic id не могут совпадать");
+    }
+
+    @Test
+    public void shouldBeEqualsIfIdsEquals() {
+        SubTask subTaskToCompare = new SubTask(0, "Помыть собаку", "Загрузить машинку", TaskStatus.IN_PROGRESS, 1);
+        Assertions.assertNotEquals(subTask, subTaskToCompare, "SubTask с одинаковым id должны быть равны");
+    }
+}
