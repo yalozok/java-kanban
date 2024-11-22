b713afeb89f74d472884eae68ece75afbd3c1973
yalozok <olga.kozmovskaia@yandex.ru>
2024-11-22 17:04:45 +0100

    Remove redundant code

--- src/task_manager/service/Main.java
+++ src/task_manager/service/Main.java
@@ -3,7 +3,6 @@ package task_manager.service;
 public class Main {
 
     public static void main(String[] args) {
-        TaskManager manager = new Manager().getDefault();
         System.out.println("Поехали!");
     }
 }
