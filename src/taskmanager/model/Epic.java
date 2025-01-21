e5fbd8e15fa5c83096c1f7781850cb9064941318
yalozok <olga.kozmovskaia@yandex.ru>
2025-01-21 18:12:47 +0100

    Update Epic

--- src/taskmanager/model/Epic.java
+++ src/taskmanager/model/Epic.java
@@ -2,6 +2,7 @@ package taskmanager.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Epic extends Task {
     private List<Integer> subTaskIds = new ArrayList<>();
@@ -23,6 +24,12 @@ public class Epic extends Task {
 
     public List<Integer> getSubTaskIds() {
         return this.subTaskIds;
+    }
+
+    public String getFormattedSubTaskIds() {
+        return subTaskIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("&"));
     }
 
     public void setSubTaskIds(List<Integer> subTaskIds) {
