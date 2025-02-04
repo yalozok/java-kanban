2fb2524b4d11b9ed498d9bb94cd175c8baa75b22
yalozok <olga.kozmovskaia@yandex.ru>
2025-02-04 14:45:56 +0100

    Clean main

--- src/taskmanager/service/Main.java
+++ src/taskmanager/service/Main.java
@@ -1,32 +1,8 @@
 package taskmanager.service;
 
-import taskmanager.model.Epic;
-import taskmanager.model.SubTask;
-import taskmanager.model.Task;
-
-import java.io.File;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Поехали!");
-
-        File file = new File("task.csv");
-
-        FileBackedTaskManager managerToFile = FileBackedTaskManager.loadFromFile(file);
-        Task task = new Task("task1", "description task1");
-        Epic epic = new Epic("epic1", "description epic1");
-        SubTask subTask = new SubTask("subtask1", "description subtask1");
-        SubTask subTask2 = new SubTask("subtask2", "description subtask2");
-
-        Integer epicId = managerToFile.addEpic(epic);
-        managerToFile.addSubTask(subTask, epicId);
-        managerToFile.addTask(task);
-        managerToFile.addSubTask(subTask2, epicId);
-
-        FileBackedTaskManager managerToFile2 = FileBackedTaskManager.loadFromFile(file);
-        System.out.println(managerToFile2.getAllTasks());
-        System.out.println(managerToFile2.getAllEpics());
-        System.out.println(managerToFile2.getAllSubTasks());
     }
 }
