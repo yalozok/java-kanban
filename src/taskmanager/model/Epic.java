a4798c583f00cec4c4292ab5547ccb7f17a9c5c1
yalozok <olga.kozmovskaia@yandex.ru>
2025-02-06 09:52:44 +0100

    Update Epic with schedule functionality

--- src/taskmanager/model/Epic.java
+++ src/taskmanager/model/Epic.java
@@ -1,28 +1,37 @@
 package taskmanager.model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Epic extends Task {
-    private List<Integer> subTaskIds;
+    private List<SubTask> subTasks;
 
     private Epic(Builder builder) {
         super(builder);
-        this.subTaskIds = builder.subTaskIds;
+        this.subTasks = builder.subTasks;
     }
 
     public static class Builder extends Task.Builder<Builder> {
-        private List<Integer> subTaskIds = new ArrayList<>();
+        private List<SubTask> subTasks = new ArrayList<>();
 
         public Builder(String name, String description) {
             super(name, description);
             this.type = TaskType.EPIC;
         }
 
-        public Builder subTaskIds(List<Integer> subTaskIds) {
-            this.subTaskIds = subTaskIds;
+        public Builder subTasks(List<SubTask> subTaskIds) {
+            this.subTasks = subTaskIds;
             return self();
+        }
+
+        @Override
+        public Builder schedule(LocalDateTime startTime, Duration duration) {
+            throw new UnsupportedOperationException("Epic's schedule is derived from its subtasks");
         }
 
         @Override
@@ -36,29 +45,85 @@ public class Epic extends Task {
         }
     }
 
-    public List<Integer> getSubTaskIds() {
-        return this.subTaskIds;
+    public List<SubTask> getSubTasks() {
+        return this.subTasks;
+    }
+
+    public void setSubTask(SubTask subTask) {
+        subTasks.add(subTask);
+        updateStatus();
+    }
+
+    public void removeSubTask(Integer id) {
+        subTasks = subTasks.stream()
+                .filter(subTask -> !subTask.getId().equals(id))
+                .collect(Collectors.toList());
+        updateStatus();
+    }
+
+    public void updateSubTask(SubTask updatedSubTask) {
+        for(int i = 0; i < subTasks.size(); i++) {
+            if(subTasks.get(i).getId().equals(updatedSubTask.getId())){
+                subTasks.set(i, updatedSubTask);
+                return;
+            }
+        }
+        updateStatus();
+    }
+
+    public void updateStatus() {
+        if(subTasks.isEmpty()) {
+            this.setStatus(TaskStatus.NEW);
+        }
+
+        Set<TaskStatus> uniqueStatuses = subTasks.stream()
+                .map(SubTask::getStatus)
+                .collect(Collectors.toSet());
+
+        this.setStatus(uniqueStatuses.size() == 1 ? uniqueStatuses.iterator().next(): TaskStatus.IN_PROGRESS);
+    }
+
+    @Override
+    public Optional<LocalDateTime> getStartTime() {
+        return subTasks.stream()
+                .map(SubTask::getStartTime)
+                .flatMap(Optional::stream)
+                .min(LocalDateTime::compareTo);
+    }
+
+    @Override
+    public Optional<Duration> getDuration() {
+        return subTasks.stream()
+                .map(SubTask::getDuration)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .reduce(Duration::plus);
+    }
+
+    @Override
+    public Optional<LocalDateTime> getEndTime() {
+        return subTasks.stream()
+                .map(SubTask::getEndTime)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .max(LocalDateTime::compareTo);
+    }
+
+    @Override
+    public void setSchedule(LocalDateTime startTime, Duration duration) {
+        throw new UnsupportedOperationException("Epic's schedule is calculated from its subtasks");
     }
 
     public String getFormattedSubTaskIds() {
-        return subTaskIds.stream()
+        return subTasks.stream()
+                .map(SubTask::getId)
                 .map(String::valueOf)
                 .collect(Collectors.joining("&"));
     }
 
-    public void setSubTaskIds(List<Integer> subTaskIds) {
-        this.subTaskIds = subTaskIds;
-    }
-
     @Override
     public String toString() {
-        return "task_manager.model.Epic{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", listSubTaskId=" + subTaskIds +
-                ", status=" + status +
-                ", type=" + type +
-                '}';
+        return super.toString() +
+                ", listSubTasks=" + subTasks;
     }
 }
--- test/EpicTest.java
+++ test/EpicTest.java
@@ -1,21 +1,25 @@
 import taskmanager.model.Epic;
+import taskmanager.model.SubTask;
 import taskmanager.model.TaskType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 class EpicTest {
     private static Epic epic;
-    private List<Integer> subtaskIds = new ArrayList<>();
+    private List<SubTask> subTasks = new ArrayList<>();
 
     @BeforeEach
     public void beforeEach() {
-        subtaskIds.add(1);
-        subtaskIds.add(2);
-        epic = new Epic.Builder("Помыть посуду", "Загрузить машинку").id(0).subTaskIds(subtaskIds).build();
+        subTasks.add(new SubTask.Builder("SubTask1", "Description SubTask1", 0).build());
+        subTasks.add(new SubTask.Builder("SubTask2", "Description SubTask2", 0).build());
+        epic = new Epic.Builder("Помыть посуду", "Загрузить машинку").id(0).subTasks(subTasks).build();
     }
 
     @Test
@@ -25,22 +29,68 @@ class EpicTest {
     }
 
     @Test
-    void shouldReturnCorrectListOfSubTaskIds() {
-        Assertions.assertEquals(subtaskIds, epic.getSubTaskIds(),
-                "Epic получает неверный список subtask ids");
+    void shouldReturnCorrectListOfSubTasks() {
+        Assertions.assertEquals(subTasks, epic.getSubTasks(),
+                "Epic получает неверный список subtask");
     }
 
     @Test
-    void shouldUpdateCorrectListOfSubTaskIds() {
-        subtaskIds.add(3);
-        epic.setSubTaskIds(subtaskIds);
-        Assertions.assertEquals(subtaskIds, epic.getSubTaskIds(),
-                "Epic получает неверный список subtask ids");
+    void shouldUpdateCorrectListOfSubTasks() {
+        SubTask subtask = new SubTask.Builder("SubTask3", "Description SubTask3", 0).build();
+        subTasks.add(subtask);
+        epic.setSubTask(subtask);
+        Assertions.assertEquals(subTasks, epic.getSubTasks(),
+                "Epic получает неверный список subtask");
     }
 
     @Test
     void shouldBeEqualsIfIdsEquals() {
-        Epic epicToCompare = new Epic.Builder( "Помыть собаку", "Загрузить машинку").id(0).subTaskIds(subtaskIds).build();
+        Epic epicToCompare = new Epic.Builder("Помыть собаку", "Загрузить машинку")
+                .id(0).subTasks(subTasks).build();
         Assertions.assertNotEquals(epic, epicToCompare, "Epic с одинаковым id должны быть равны");
     }
+
+    @Test
+    void shouldNotTakeTimeAndDurationForCreation() {
+        LocalDateTime startTime = LocalDateTime.now();
+        Duration duration = Duration.ofMinutes(10);
+        Assertions.assertThrows(UnsupportedOperationException.class, new Epic.Builder("EpicWithData", "DescriptionEpicWithData")
+                .schedule(startTime, duration)::build, "Epic принимает время и дату в конструкторе.");
+    }
+
+    @Test
+    void shouldNotSetScheduleByItself() {
+        LocalDateTime startTime = LocalDateTime.now();
+        Duration duration = Duration.ofMinutes(10);
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> epic.setSchedule(startTime, duration), "Epic может установить время, дату и продолжительность через setter");
+    }
+
+    @Test
+    void shouldNotReturnGetEndTimeWithEmptySubTasksList() {
+        Epic epicWithEmptySubtasks = new Epic.Builder("EpicWithEmptySubtasks", "DescriptionEpicWithEmptySubtasks")
+                .build();
+        Assertions.assertEquals(Optional.empty(), epicWithEmptySubtasks.getEndTime(),
+                "Epic может получить время окончания задачи с пустым списком subtasks");
+        Assertions.assertEquals(Optional.empty(), epic.getEndTime(),
+                "Epic может получить время окончания задачи с subtasks без schedules");
+    }
+
+    @Test
+    void calculateCorrectSchedule() {
+        LocalDateTime startTimeSubTask3 = LocalDateTime.now();
+        LocalDateTime startTimeSubTask4 = LocalDateTime.now().plusMinutes(5);
+        Duration durationSubTask3 = Duration.ofMinutes(10);
+        Duration durationSubTask4 = Duration.ofMinutes(20);
+
+        SubTask subTask3 = new SubTask.Builder("SubTask3", "DescriptionSubTask3", 0)
+                .schedule(startTimeSubTask3, durationSubTask3).build();
+        SubTask subTask4 = new SubTask.Builder("SubTask4", "DescriptionSubTask4", 0)
+                .schedule(startTimeSubTask4, durationSubTask4).build();
+        epic.setSubTask(subTask3);
+        epic.setSubTask(subTask4);
+        Assertions.assertEquals(Optional.of(startTimeSubTask3), epic.getStartTime(), "Старт Epic вычисляется неверно");
+        Assertions.assertEquals(Optional.of(durationSubTask3.plus(durationSubTask4)), epic.getDuration(), "Продолжительность Epic вычисляется неверно");
+        Assertions.assertEquals(subTask4.getEndTime(), epic.getEndTime(), "Финиш Epic вычисляется неверно");
+    }
+
 }
